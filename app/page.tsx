09665bf3d64bafb93cfbbe0817979fcd76733aff
Nguyen Mai Minh Quy <maiminhquyn@gmail.com>
2025-07-31 08:05:41 +0700

    add '.ts' extension in file layout.tsx & page.tsx for '@/app/ui/font.ts'

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/font';
+import { lusitana } from '@/app/ui/font.ts';
 import Image from 'next/image';
 import styles from '@/app/ui/home.module.css';
 import Link from 'next/link';
